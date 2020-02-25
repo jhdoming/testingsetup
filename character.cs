8fcc0b8e464bba3ae737d7d6590c4c2a4b54c062
Max Hernandez <mhern120@asu.edu>
2020-02-25 14:34:30 -0700

    Create character.cs

---
+++ character.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Character
+{
+    private string _charName;
+    private string _class;
+    private string _race;
+    private string _attackOne;
+    private string _attackTwo;
+    private string _attackThree;
+    
+    private int _level;
+    private int _str;
+    private int _dex;
+    private int _wis;
+    private int _cha;
+    private int _armorClass;
+    private list<string> equipment = new List<string>();
+    
+
+
+
+}
