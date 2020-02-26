38ac318d5b8fd2adaaf9e4b9e04cc8384b1db38a
Max Hernandez <mhern120@asu.edu>
2020-02-26 01:31:38 -0700

    fixing makeup of class

---
+++ ProjectTemplate/Character.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ProjectTemplate
+{
+    public class Character
+    {
+
+        public string _charName {get; set;}
+        public string _class {get; set;}
+        public string _race {get; set;}
+        public string _attackOne {get; set;}
+        public string _attackTwo {get; set;}
+        public string _attackThree {get; set;}
+        public int _level {get; set;}
+        public int _str {get; set;}
+        public int _dex {get; set;}
+        public int _wis {get; set;}
+        public int _cha {get; set;}
+        public int _armorClass {get; set;}
+        public string[] _equipment {get; set;}
+        public string[] _otherProf {get; set;}
+        public string[] _languages {get; set;}
+        public string[] _knownSkills {get; set;}
+
+    }
+}
