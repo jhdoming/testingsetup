1b87f0f631350f55fbb5f62333799bd126cbc75d
Max Hernandez <mhern120@asu.edu>
2020-02-26 21:51:23 -0700

    Condensed JS into one File

---
+++ ProjectTemplate/script.js
@@ -0,0 +1,79 @@
+function postCharacter() 
+{
+    var data = jsonRetun.data.children[i].data;
+
+    $.ajax({
+        type: "POST",
+        url: webmethod,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(msg, charName) {
+            
+        }
+    })
+}
+
+function createAccount() {
+    var id = document.getElementById("createId").value;
+    var pass = document.getElementById("createPass").value;
+
+    var webMethod = "ProjectServices.asmx/createAccount";
+
+    var parameters = "{\"uid\":\"" + encodeURI(id) + "\", \"pass\":\"" + encodeURI(pass) + "\"}";
+
+
+    //jQuery ajax method
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        data: parameters,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            var responseFromServer = msg.d;
+            if (responseFromServer == true) {
+                alert(responseFromServer);
+                //window.location.replace("index.html");
+            }
+            else {
+                alert(responseFromServer);
+            }
+            //after creating an account, sends you back to the login page (works here but also works when password isnt set right)
+            //window.location.replace("index.html");
+        },
+        error: function (e) {
+            alert("this code will only execute if javascript is unable to access the webservice");
+        }
+    });
+}
+
+function logon() {
+    var id = document.getElementById("logonId").value;
+    var pass = document.getElementById("logonPass").value;
+
+    var webMethod = "ProjectServices.asmx/LogOn";
+
+    var parameters = "{\"uid\":\"" + encodeURI(id) + "\", \"pass\":\"" + encodeURI(pass) + "\"}";
+
+
+    //jQuery ajax method
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        data: parameters,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            var responseFromServer = msg.d;
+            if (responseFromServer == true) {
+                location.href ="/mainpage.html";
+            }
+            else {
+                alert("bad credentials");
+            }
+        },
+        error: function (e) {
+            alert("this code will only execute if javascript is unable to access the webservice");
+        }
+    });
+}
