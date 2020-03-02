103220fc1fac4b3ddab691e3f510d4e6bff063a5
jhdoming <60128529+jhdoming@users.noreply.github.com>
2020-03-01 19:05:52 -0700

    createCharJsCode

--- ProjectTemplate/script.js
+++ ProjectTemplate/script.js
@@ -32,6 +32,66 @@ function createAccount() {
     });
 }
 
+
+function createCharacter() {
+    var charName = document.getElementById("newNameId").value;
+    var charClass = document.getElementById("newClassId").value;
+    var charRace = document.getElementById("newRaceId").value;
+    var charLevel = document.getElementById("newLevelId").value;
+
+    var charHealth = document.getElementById("newHealthId").value;
+    var charStrength = document.getElementById("newStrengthId").value;
+    var charDexterity = document.getElementById("newDexterityId").value;
+    var charConstitution = document.getElementById("newConsitutionId").value;
+
+    var charIntelligence = document.getElementById("newIntelligenceId").value;
+    var charWisdom = document.getElementById("newWisdomId").value;
+    var charCharisma = document.getElementById("newCharismaId").value;
+
+    var charAttack1 = document.getElementById("newAttack1Id").value;
+    var charAttack2 = document.getElementById("newAttack2Id").value;
+    var charAttack3 = document.getElementById("newAttack3Id").value;
+
+    var charArmorclass = document.getElementById("newArmorclassId").value;
+
+    var charEquipment = document.getElementById("newEquipmentId").value;
+    var charOtherProficiency = document.getElementById("newOtherproficiencyId").value;
+
+    var charLanguage = document.getElementById("newLanguageId").value;
+    //figure out how to loop through all checkboxes and concatenate all values and save in a variable
+    var charSkills = document.getElementById("newAttack2Id").value;
+    var charKnownSaves = document.getElementById("newKnownsavesId").value;
+    
+
+    var webMethod = "AccountServices.asmx/RequestAccount";
+    var parameters = "{\"CharName\":\"" + encodeURI(charName) + "\",\"Class\":\"" + encodeURI(charClass) +
+                        "\",\"Race\":\"" + encodeURI(charRace) + "\",\"Level\":\"" + encodeURI(charLevel) +
+                        "\",\"Health\":\"" + encodeURI(charHealth) + "\",\"Str\":\"" + encodeURI(charStrength) +
+                        "\",\"Dex\":\"" + encodeURI(charDexterity) + "\",\"Con\":\"" + encodeURI(charConstitution) +
+                        "\",\"Int\":\"" + encodeURI(charIntelligence) + "\",\"Wis\":\"" + encodeURI(charWisdom) +
+                        "\",\"Cha\":\"" + encodeURI(charCharisma) + "\",\"AttackOne\":\"" + encodeURI(charAttack1) +
+                        "\",\"AttackTwo\":\"" + encodeURI(charAttack2) + "\",\"AttackThree\":\"" + encodeURI(charAttack3) +
+                        "\",\"ArmorClass\":\"" + encodeURI(charArmorclass) + "\",\"Equipment\":\"" + encodeURI(charEquipment) +
+                        "\",\"OtherProf\":\"" + encodeURI(charOtherProficiency) + "\",\"Languages\":\"" + encodeURI(charLanguage) +
+                        "\",\"KnownSkills\":\"" + encodeURI(charSkills) + "\",\"KnownSaves\":\"" + encodeURI(charKnownSaves) +"\"}";
+
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        data: parameters,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            //showPanel('logonPanel');
+            alert("New Character has been Created");
+        },
+        error: function (e) {
+            alert("Error: something went wrong");
+        }
+    });
+}
+
+
 function logon() {
     var id = document.getElementById("logonId").value;
     var pass = document.getElementById("logonPass").value;
