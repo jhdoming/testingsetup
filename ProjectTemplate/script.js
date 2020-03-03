c1be14eba39a0aa3356a7274ac65e41367b4787e
juliapagliaro <julia.pagliaro@asu.edu>
2020-03-02 18:24:53 -0700

    Needs Review

--- ProjectTemplate/script.js
+++ ProjectTemplate/script.js
@@ -64,8 +64,11 @@ function logon() {
     });
 }
 
-function postCharacter() 
+function postCharacter1() 
 {
+
+    window.location.href = 'characterSheet.html'
+    
     var webMethod = "ProjectServices.asmx/GetCharacters";
     var characterArray;
     var characterID;
@@ -78,6 +81,93 @@ function postCharacter()
         success: function(data) {
             characterArray = data;
             characterID = 0;
+            $("#charName").val(characterArray.d[characterID]._charName);
+            $("#class").val(characterArray.d[characterID]._class);
+            $("#level").val(characterArray.d[characterID]._level);
+            $("#armorClass").val(characterArray.d[characterID]._armorClass);
+            $("#otherProf").val(characterArray.d[characterID]._otherProf);
+            $("#str").val(characterArray.d[characterID]._str);
+            $("#dex").val(characterArray.d[characterID]._dex);
+            $("#con").val(characterArray.d[characterID]._con);
+            $("#int").val(characterArray.d[characterID]._int);
+            $("#wis").val(characterArray.d[characterID]._wis);
+            $("#cha").val(characterArray.d[characterID]._cha);
+
+        }
+    })
+}
+
+function postCharacter2() {
+    var webMethod = "ProjectServices.asmx/GetCharacters";
+    var characterArray;
+    var characterID;
+
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            characterArray = data;
+            characterID = 1;
+            $("#charName").val(characterArray.d[characterID]._charName);
+            $("#class").val(characterArray.d[characterID]._class);
+            $("#level").val(characterArray.d[characterID]._level);
+            $("#armorClass").val(characterArray.d[characterID]._armorClass);
+            $("#otherProf").val(characterArray.d[characterID]._otherProf);
+            $("#str").val(characterArray.d[characterID]._str);
+            $("#dex").val(characterArray.d[characterID]._dex);
+            $("#con").val(characterArray.d[characterID]._con);
+            $("#int").val(characterArray.d[characterID]._int);
+            $("#wis").val(characterArray.d[characterID]._wis);
+            $("#cha").val(characterArray.d[characterID]._cha);
+
+        }
+    })
+}
+
+function postCharacter3() {
+    var webMethod = "ProjectServices.asmx/GetCharacters";
+    var characterArray;
+    var characterID;
+
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            characterArray = data;
+            characterID = 2;
+            $("#charName").val(characterArray.d[characterID]._charName);
+            $("#class").val(characterArray.d[characterID]._class);
+            $("#level").val(characterArray.d[characterID]._level);
+            $("#armorClass").val(characterArray.d[characterID]._armorClass);
+            $("#otherProf").val(characterArray.d[characterID]._otherProf);
+            $("#str").val(characterArray.d[characterID]._str);
+            $("#dex").val(characterArray.d[characterID]._dex);
+            $("#con").val(characterArray.d[characterID]._con);
+            $("#int").val(characterArray.d[characterID]._int);
+            $("#wis").val(characterArray.d[characterID]._wis);
+            $("#cha").val(characterArray.d[characterID]._cha);
+
+        }
+    })
+}
+
+function postCharacter4() {
+    var webMethod = "ProjectServices.asmx/GetCharacters";
+    var characterArray;
+    var characterID;
+
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            characterArray = data;
+            characterID = 3;
             $("#charName").val(characterArray.d[characterID]._charName);
             $("#class").val(characterArray.d[characterID]._class);
             $("#level").val(characterArray.d[characterID]._level);
